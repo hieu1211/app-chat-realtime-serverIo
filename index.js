e5ded09f7300b79acf54ad25c0d22322d2231a03
unknown <hieu73600@st.vimaru.edu.vn>
2020-09-09 15:27:11 +0700

    create Users-store and socket-messenger

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const socketio = require('socket.io')
 const http = require('http');
+const { allowedNodeEnvironmentFlags } = require('process');
 const server = http.createServer(app);
 const io = socketio(server);
+const users = require('./users/users');
 server.listen(5000,()=> console.log("server started on port 5000"))
 
 app.get('/',(req,res)=>{
@@ -11,7 +13,20 @@ app.get('/',(req,res)=>{
 })
 
 io.on('connection',(socket)=>{
-    console.log(`${socket.id} has connected`)
+    
+    socket.on("join",({name,room})=>{
+        users.add(socket.id,name,room);
+        socket.join(room);
+        console.log(`${socket.id} has connected`)
+    })
+
+    socket.emit("messenger",{name:"admin",mes:"Welcome to the room"})
+    
+    socket.on("messenger",(data)=>{
+        let user = users.getUser(socket.id);
+        socket.broadcast.to(user.room).emit("messenger",{name:user.name,mes:data})
+    })
+
     socket.on("disconnect",()=>{
         console.log(`${socket.id} has disconnected`)
     })
---
+++ users/users.js
@@ -0,0 +1,22 @@
+let users = [];
+module.exports = {
+    add: (id,name,room)=>{
+        users.push({id,name,room});
+    },
+
+    getUser: (id) =>{
+        return users.find((user)=>user.id === id);
+    },
+
+    getAllUsersOfRoom: (room) =>{
+        return users.filter((user)=>{
+            user.room === room;
+        })
+    },
+
+    RemoveUser: (id)=>{
+        const user = users.find((user)=>user.id === id);
+        const index = users.indexOf(user);
+        users.splice(index,1);
+    }
+}
